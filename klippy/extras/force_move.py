cecd538dceb1900075e6c113760343ef8d3d3c6b
ludovico26 <93492423+ludovico26@users.noreply.github.com>
2021-11-15 16:40:36 +0100

    Update force_move.py

--- klippy/extras/force_move.py
+++ klippy/extras/force_move.py
@@ -33,6 +33,7 @@ class ForceMove:
         self.printer = config.get_printer()
         self.steppers = {}
         self.enable_lines = {}
+        self.name= config.get_name()
         # Setup iterative solver
         ffi_main, ffi_lib = chelper.get_ffi()
         self.trapq = ffi_main.gc(ffi_lib.trapq_alloc(), ffi_lib.trapq_free)
@@ -54,6 +55,9 @@ class ForceMove:
                                    desc=self.cmd_MODIFY_ROTATION_help)
             gcode.register_command('SET_STEP_DIST', self.cmd_SET_STEP_DIST,
                                    desc=self.cmd_SET_STEP_DIST_help)
+            gcode.register_mux_command('CHANGE_STEP_DIST',"STEPPER",
+                                   self.name, self.cmd_CHANGE_STEP_DIST,
+                                   desc=self.cmd_CHANGE_STEP_DIST)
     def register_stepper(self, config, mcu_stepper):
         self.steppers[mcu_stepper.get_name()] = mcu_stepper
     def lookup_stepper(self, name):
@@ -162,6 +166,20 @@ class ForceMove:
         configfile.set(stepper_name, 'rotation_distance',"%.3f" % (dist,))
         gcmd.respond_info("stepper_z ' rotation distance set to %0.6f"
                           % (dist))
+    cmd_CHANGE_STEP_DIST_help = "Modify stepper step distance"
+    def cmd_CHANGE_STEP_DIST(self, gcmd):
+        dist = gcmd.get_float('DISTANCE', None, above=0.)
+        toolhead = self.printer.lookup_object('toolhead')
+        dist = gcmd.get_float('DISTANCE', None, above=0.)
+        if dist is None:
+            step_dist = self.stepper.get_step_dist()
+            gcmd.respond_info("Extruder '%s' step distance is %0.6f"
+                              % (self.name, step_dist))
+            return
+        toolhead.flush_step_generation()
+        #self.stepper.set_step_dist(dist)
+        gcmd.respond_info("Extruder '%s' step distance set to %0.6f"
+                          % (self.name, dist))  
     cmd_SET_KINEMATIC_POSITION_help = "Force a low-level kinematic position"
     def cmd_SET_KINEMATIC_POSITION(self, gcmd):
         toolhead = self.printer.lookup_object('toolhead')
