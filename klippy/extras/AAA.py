0094902f8e3ee96597674fc7518543504246a6c0
ludovico26 <93492423+ludovico26@users.noreply.github.com>
2021-11-16 16:31:23 +0100

    Create AAA.py

---
+++ klippy/extras/AAA.py
@@ -0,0 +1,41 @@
+import math, logging
+import stepper, chelper
+
+class ModifyRotation:
+    def __init__(self, config, extruder_num):
+        self.printer = config.get_printer()
+        self.name = config.get_name()
+        toolhead = self.printer.lookup_object('toolhead')
+        self.stepper = stepper.PrinterStepper(config)
+        toolhead = self.printer.lookup_object('toolhead')
+        if self.name == 'stepper_z':
+            gcode.register_mux_command("SET_EXTRUDER_STEP_DISTANCE", "STEPPER_Z",
+                                   self.name, self.cmd_SET_Z_DISTANCE,
+                                   desc=self.cmd_SET_Z_DISTANCE_help)
+    cmd_SET_Z_DISTANCE_help = "Set extruder step distance"
+    def cmd_SET_Z_DISTANCE(self, gcmd):
+        toolhead = self.printer.lookup_object('toolhead')
+        dist = gcmd.get_float('DISTANCE', None, above=0.)
+        if dist is None:
+            step_dist = self.stepper.get_step_dist()
+            gcmd.respond_info("Stepper_z '%s' step distance is %0.6f"
+                              % (self.name, step_dist))
+            return
+        toolhead.flush_step_generation()
+        self.stepper.set_step_dist(dist)
+        gcmd.respond_info("Stepper_z '%s' step distance set to %0.6f"
+                          % (self.name, dist))
+    def add_printer_objects(config):
+    printer = config.get_printer()
+    for i in range(99):
+        section = 'stepper_z'
+        if i:
+            section = 'stepper_z%d' % (i,)
+        if not config.has_section(section):
+            break
+        pe = ModifyRotation(config.getsection(section), i)
+        printer.add_object(section, pe)
+    def get_name(self):
+        return self.name
+def load_config(config):
+    return ModifyRotation(config)
