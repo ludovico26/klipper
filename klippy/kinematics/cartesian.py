1fd89d898e8ca1a6a9f3113f15ef7ecdc14c7c91
ludovico26 <93492423+ludovico26@users.noreply.github.com>
2021-11-16 11:48:44 +0100

    Update cartesian.py

--- klippy/kinematics/cartesian.py
+++ klippy/kinematics/cartesian.py
@@ -33,9 +33,6 @@ class CartKinematics:
         ranges = [r.get_range() for r in self.rails]
         self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.)
         self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.)
-        gcode.register_mux_command("SET_MY_STEP_DISTANCE", "STEPPER",
-                                   self.name, self.cmd_MY_STEP_DISTANCE,
-                                   desc=self.cmd_MY_STEP_DISTANCE_help)
         # Check for dual carriage support
         if config.has_section('dual_carriage'):
             dc_config = config.getsection('dual_carriage')
@@ -142,18 +139,5 @@ class CartKinematics:
     def cmd_SET_DUAL_CARRIAGE(self, gcmd):
         carriage = gcmd.get_int('CARRIAGE', minval=0, maxval=1)
         self._activate_carriage(carriage)
-    cmd_MY_STEP_DISTANCE_help = "Set extruder step distance"
-    def cmd_MY_STEP_DISTANCE(self, gcmd):
-        toolhead = self.printer.lookup_object('toolhead')
-        dist = gcmd.get_float('DISTANCE', None, above=0.)
-        if dist is None:
-            step_dist = self.stepper.get_step_dist()
-            gcmd.respond_info("Stepper '%s' step distance is %0.6f"
-                              % (self.name, step_dist))
-            return
-        toolhead.flush_step_generation()
-        #self.stepper.set_step_dist(dist)
-        gcmd.respond_info("Stepper '%s' step distance set to %0.6f"
-                          % (self.name, dist))
 def load_kinematics(toolhead, config):
     return CartKinematics(toolhead, config)
