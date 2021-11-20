767b1ecd198a699a0d17da26af788f64b7fc4436
ludovico26 <93492423+ludovico26@users.noreply.github.com>
2021-11-20 14:56:09 +0100

    Update z_tilt.py

--- klippy/extras/z_tilt.py
+++ klippy/extras/z_tilt.py
@@ -146,9 +146,9 @@ class ZTilt:
     cmd_MODIFY_PROBE_help="modify porbe pt"
     def cmd_MODIFY_PROBE(self,gcmd):
         logging.info("modifying probe points")
-        offset=[]
-        offset[0]= gcmd.get_float('A', 0., minval=-10,maxval=30)
-        self.z_postions[0][0]=offset[0]
+        #offset=[] offset[0]
+        #offset[0]= gcmd.get_float('A', 0., minval=-10,maxval=30)
+        self.z_postions[0][0]=gcmd.get_float('A', 0., minval=-10,maxval=30)
         gcmd.respond_info("modifying first z offset %.3f",
                           self.z_positions[0][0])
     def update_z_positions(self, points, min_points):
