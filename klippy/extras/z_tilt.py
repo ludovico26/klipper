cf26eb8f994523179725775d9b5e9c03fe7a2693
ludovico26 <93492423+ludovico26@users.noreply.github.com>
2021-11-23 11:14:05 +0100

    Update z_tilt.py

--- klippy/extras/z_tilt.py
+++ klippy/extras/z_tilt.py
@@ -148,7 +148,9 @@ class ZTilt:
         #offset=[] offset[0]
         #offset[0]= gcmd.get_float('A', 0., minval=-10,maxval=30)
         #self.z_postions[1][2]=gcmd.get_float('A', 0., minval=-10,maxval=30)
-        gcmd.respond_info("modifying first z offset %.3f",
+        #gcmd.respond_info("modifying first z offset %.3f",
+        #               self.z_positions[1][1])
+        logging.info("modifying first z offset %.3f",
                        self.z_positions[1][1])
     def update_z_positions(self, points, min_points):
         self.z_positins = points
